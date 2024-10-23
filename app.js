a2ab3bfb1fdf6628d7193792c51fe80e699d8a04
Waghre Abhishek <waghreabhishek@gmail.com>
2024-10-23 22:50:19 +0530

    Portfolio

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ window.onload = function () {
     gsap.from(".nav-i", {
         y: -100,    // Slide from above
         opacity: 0, // Fade in from opacity 0
-        duration: 3.2, // 1 second animation
+        duration: 2.5, // 1 second animation
         ease: "power2.out" // Smooth easing
     });
 };
